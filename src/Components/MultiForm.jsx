9c53f96a73b505c6e3a8cfd80f128f933c6ff710
Rahul Kumar <rahul@promactinfo.com>
2024-01-01 14:25:00 +0530

    Handling Form Submission In React

--- src/Components/MultiForm.jsx
+++ src/Components/MultiForm.jsx
@@ -2,8 +2,13 @@ import { useState } from "react";
 
 export default function MultiForm() {
   const [name, setValue] = useState({ firstName: "", lastName: "" });
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(name);
+  }
   return (
     <div>
+      {name.firstName + " - " + name.lastName}
       <form>
         <input
           onChange={(e) => setValue({ ...name, firstName: e.target.value })}
@@ -17,6 +22,7 @@ export default function MultiForm() {
           placeholder="Last Name"
           value={name.lastName}
         />
+        <button onClick={(e) => handleSubmit(e)}>Add</button>
       </form>
     </div>
   );
